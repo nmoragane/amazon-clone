dea35fee340154141cdfebd65abcfbfeaea7cdf3
Nayomal Lakshitha <nmoragane@gmail.com>
2021-01-27 23:42:25 +0530

    products

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,58 @@
 import React from 'react';
 import '../styles/home.css';
+import Product from './Product';
 
-function Home(props) {
+function Home() {
     return (
         <div className="home">
             <div className="home__container">
                 <img className="home__image"
                 src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
                  alt=""/>
+
+                 <div className="home__row">
+                     <Product
+                     title="The Lean Startup"
+                     price={29.99}
+                     image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+                     rating={5}/>
+
+                     <Product
+                     title="The Lean Startup"
+                     price={29.99}
+                     image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+                     rating={5}/>
+                 </div>
+
+                 <div className="home__row">
+                    <Product
+                     title="The Lean Startup"
+                     price={29.99}
+                     image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+                     rating={5}/>
+                     
+                    <Product
+                     title="The Lean Startup"
+                     price={29.99}
+                     image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+                     rating={5}/>
+
+                    <Product
+                     title="The Lean Startup"
+                     price={29.99}
+                     image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+                     rating={5}/>
+                     
+                 </div>
+
+                 <div className="home__row">
+                     <Product
+                     title="The Lean Startup"
+                     price={29.99}
+                     image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+                     rating={5}/>               
+                 </div>
+
             </div>
         </div>
     );

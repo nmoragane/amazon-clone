a6541346c1c36ad3f066d3121d2a670c48766894
Nayomal Lakshitha <nmoragane@gmail.com>
2021-02-07 23:03:26 +0530

    setup strip init

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/payment.css'
 import CheckoutProducts from './CheckoutProducts';
 import { useStateValue } from '../StateProvider';
@@ -6,6 +6,7 @@ import {Link} from "react-router-dom";
 import { getBasketTotal } from '../reducer';
 import { useElements, useStripe, CardElement } from '@stripe/react-stripe-js';
 import CurrencyFormat from 'react-currency-format';
+import axios from '../axios';
 
 function Payment(props) {
     const [{basket, user}, dispatch] = useStateValue();
@@ -13,11 +14,43 @@ function Payment(props) {
     const stripe = useStripe();
     const element = useElements();
 
+    const [succeeded, setSucceeded] = useState(false);
+    const [processing, setProcessing] = useState("");
     const [error, setError] = useState(null);
     const [disabled, setDisabled] = useState(true);
+    const [clientSecret, setClientSecret] = useState(true);
 
-    const handleSubmit = e => {
+    useEffect (() => {
+        //generate the special stripe func which allows us to charge the customer
+        const getClientSecret = async () => {
+            const respose = await axios({
+                method:'post',
+                url: `/payment/create?total${getBasketTotal(basket) * 100}`
+            });
+            setClientSecret(respose.data.clientSecret)
+        }
+        getClientSecret()
+    }, [basket])
+    
+    
 
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setProcessing(true);
+
+        const payload = await stripe.confirmCardPayment(clientSecret, {
+            payment_method : {
+                card: element.getElement(CardElement)
+            }
+        }).then(({paymentIntent}) => {
+            // paymentIntent = payment confirmation
+
+            setSucceeded(true)
+            setError(null)
+            setProcessing(false)
+
+            history.replaceState('/orders')
+        })
     }
 
     const handleChange = e => {
@@ -83,6 +116,10 @@ function Payment(props) {
                                 thousandSeparator={true}
                                 prefix={"$"}
                                 />
+
+                                <button disabled={processing || disabled ||succeeded}>
+                                    <span>{processing ? <p>Processing...</p>: "Buy Now"}</span>
+                                </button>
                             </div>
                         </form>
                     </div>

ba9a0307e46de1a282547c32e254b1857d7a7fe9
Nayomal Lakshitha <nmoragane@gmail.com>
2021-02-09 22:44:32 +0530

    stripe finish

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,37 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")('sk_test_51IHCQbBMf5KAvc4xq7GAB7HptI89gMOJ0x99WoOQzTX1GmojWybIbrsLHJsYIdazW1GhnEW6Y4m2p9T9a7ybfmIX00wFHTE0gC');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// API
+
+// /app config
+const app = express();
+
+// middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// api routes
+app.get('/', (req,res) => res.status(200).send('hellow'));
+
+app.post('/payments/create', async (req,res) => {
+    const total = req.query.total;
+
+    console.log('payment req recieved >>> ', total);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total, //subunits usd => cents
+        currency: "usd",
+    });
+
+    res.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+    })
+} )
+
+// listen command
+exports.api = functions.https.onRequest(app);
+
+// exmple endpoint
+// http://localhost:5001/clone-838ef/us-central1/api
